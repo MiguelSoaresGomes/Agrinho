4fbe4eba662997aaff7ff06ceefc866455f771a6
MiguelSoaresGomes <107508726+MiguelSoaresGomes@users.noreply.github.com>
2024-07-04 08:38:25 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+function exibirAlerta1() {
+ alert('Certamente! A agropecuária é um setor vital para a economia brasileira, representando cerca de 20% do PIB. Ela envolve plantio e criação de animais, gerando empregos e matérias-primas essenciais. 🌾🐄');
+
+}
+function exibirAlerta2() {
+alert('A agropecuária é uma atividade econômica do tipo primária que envolve o cultivo da terra e a criação de animais. Esse setor é um dos mais importantes da economia brasileira. A agropecuária é caracterizada  tanto pelo cultivo de plantas quanto pela criação de animais.'); }
+
+ function exibirAlerta3() {
+ alert('AGRO É BRAZIL'); }  
+
+function exibirAlerta4() {
+ alert('AGRO É BRAZIL'); }  
+
+ 
