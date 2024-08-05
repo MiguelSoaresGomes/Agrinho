fbfe754d4132cb241f2635263a669ab4458f13db
MiguelSoaresGomes <107508726+MiguelSoaresGomes@users.noreply.github.com>
2024-08-05 07:55:41 -0300

    Update java.js
    30

--- java.js
+++ java.js
@@ -3,12 +3,12 @@ function exibirAlerta1() {
 
 }
 function exibirAlerta2() {
-alert('A agropecuária é uma atividade econômica do tipo primária que envolve o cultivo da terra e a criação de animais. Esse setor é um dos mais importantes da economia brasileira. A agropecuária é caracterizada  tanto pelo cultivo de plantas quanto pela criação de animais.'); }
+alert('A agropecuária é uma atividade econômica do tipo primária que envolve o cultivo da terra e a criação de animais. Esse setor é um dos mais importantes da economia brasileira. A agropecuária é caracterizada  tanto pelo cultivo de plantas quanto pela criação de animais.👨‍🌾🐮👨‍🌾'); }
 
  function exibirAlerta3() {
- alert('AGRO É BRAZIL'); }  
+ alert('A agricultura envolve o plantio e cultivo de vegetais, como grãos, frutas, legumes e hortaliças.É praticada em pequenas, médias e grandes propriedades.Objetivos: subsistência e/ou comercialização.Atende tanto ao mercado doméstico quanto à demanda internacional por produtos primários. Exemplo: cultivo de soja, carro-chefe da produção no Brasil.🐂🐄'); }  
 
 function exibirAlerta4() {
- alert('AGRO É BRAZIL'); }  
+ alert('A pecuária envolve a criação de animais, como gado, porcos, aves e cavalos.   Também praticada em propriedades rurais.   Objetivos: produção de carne, leite, couro etc.🦬 '); }  
 
  
